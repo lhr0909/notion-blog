42b79acb599b4d934883727842ec2dbe442a0a91
Simon Liang <no.lhr0909@gmail.com>
2022-02-04 22:39:50 +0800

    added more social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,27 +4,63 @@ import React from "react";
 import GithubIcon from "./icons/GitHubIcon";
 import LinkedInIcon from "./icons/LinkedInIcon";
 import TwitterIcon from "./icons/TwitterIcon";
+import YoutubeIcon from "./icons/YouTubeIcon";
+import BilibiliIcon from "./icons/BilibiliIcon";
 
 export const Footer: React.FC<{}> = () => {
   return (
     <footer className="text-gray-500 font-extralight my-8 flex flex-col sm:flex-row sm:justify-between">
-      <span className="block">
-        Copyright (c) 2022 Simon Liang
-      </span>
+      <span className="block">Copyright (c) 2022 Simon Liang</span>
       <span className="flex flex-row mt-2 sm:mt-0">
         <Link href="https://github.com/lhr0909" passHref>
-          <a className="hover:text-blue-500 mr-1" target="_blank" rel="noopener">
+          <a
+            className="hover:text-blue-500 mr-1"
+            target="_blank"
+            rel="noopener"
+          >
             <GithubIcon className="h-6 w-6" />
           </a>
         </Link>
         <Link href="https://twitter.com/lhr0909" passHref>
-          <a className="hover:text-blue-500 mx-1" target="_blank" rel="noopener">
+          <a
+            className="hover:text-blue-500 mx-1"
+            target="_blank"
+            rel="noopener"
+          >
             <TwitterIcon className="h-6 w-6" />
           </a>
         </Link>
         <Link href="https://www.linkedin.com/in/simon-liang" passHref>
-          <a className="hover:text-blue-500 ml-1" target="_blank" rel="noopener">
+          <a
+            className="hover:text-blue-500 mx-1"
+            target="_blank"
+            rel="noopener"
+          >
             <LinkedInIcon className="h-6 w-6" />
+          </a>
+        </Link>
+        <Link
+          href="https://www.youtube.com/channel/UC0VobXaEqKPcUab9rKihytA"
+          passHref
+        >
+          <a
+            className="hover:text-blue-500 mx-1"
+            target="_blank"
+            rel="noopener"
+          >
+            <YoutubeIcon className="h-6 w-6" />
+          </a>
+        </Link>
+        <Link
+          href="https://space.bilibili.com/41930008"
+          passHref
+        >
+          <a
+            className="hover:text-blue-500 ml-1"
+            target="_blank"
+            rel="noopener"
+          >
+            <BilibiliIcon className="h-6 w-6" />
           </a>
         </Link>
       </span>
