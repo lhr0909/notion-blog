3c7d96c75131d61cae6dde99b324fda6d13344ca
Simon Liang <no.lhr0909@gmail.com>
2022-05-14 23:50:30 +0800

    updated button text

--- pages/card.tsx
+++ pages/card.tsx
@@ -60,7 +60,7 @@ const Home: NextPage<HomeProps> = ({ siteTitle, introHtml, posts }) => {
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base cursor-pointer font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
               onClick={() => openQRCode()}
             >
-              开始试用
+              点这里联系我
             </button>
           </div>
         </article>
