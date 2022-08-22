25b8635c8704deb2b8b2ea11a8532206e005860e
Simon Liang <no.lhr0909@gmail.com>
2022-08-22 14:54:00 +0800

    try 365 days and see if it gets better

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
   images: {
     // whitelist notion images domain
     domains: ['s3.us-west-2.amazonaws.com'],
-    minimumCacheTTL: 60 * 60 * 24 * 3, // 3 days
+    minimumCacheTTL: 60 * 60 * 24 * 365, // 365 days
   },
-  async redirects() {
+  redirects() {
     return [
       {
         source: '/poker/2017/09/03/run-equilab-on-macos-with-winebottler.html',
